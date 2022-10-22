9c5024b5efcd760ba80db30ec461e355a9327e35
Nafisa <nafiritzy.59@gmail.com>
2022-10-22 11:34:36 +0530

    initialize menu header and background

---
+++ src/home.js
@@ -0,0 +1,6 @@
+function homediv(){
+    const div= document.createElement('div');
+    div.style.cssText="background-colour:yellow; width:70%; height:40%";
+}
+
+export homediv();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("It's workingkkkk");
+import './style.css';
+import homediv from './home.js';
+console.log("It's wo");
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+   
+ element.style.cssText="background-colour:white; width:75%; height:45%";
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
